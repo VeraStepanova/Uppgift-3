8de2425e6018ef205e9e74383d723277197298c0
Vera Stepanova <stepa.vera@gmail.com>
2020-11-01 19:42:36 +0100

    method 3

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,16 @@
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
 
 public class Book {
-    private static Book book;
     String title;
     String author;
     double price;
     LocalDate dateOfLoan;
 
-public static String getName(Book book){ return "Titel: " + book.title + "\nAuthor:  " + book.author +"\nPrice: " +  book.price;}
+public static String getName(Book book){
+    return "Titel: " + book.title +
+            "\nAuthor:  " + book.author +
+            "\nPrice: " +  book.price;}
 
 public static long daysOfLoan(Book book) {
     LocalDate today = LocalDate.now();
@@ -18,10 +18,9 @@ public static long daysOfLoan(Book book) {
     //LocalDate dateOfLoan = LocalDate.of(2020, Month.OCTOBER, 20 );
     System.out.println("Day of loan: " + book.dateOfLoan);
     return ChronoUnit.DAYS.between(book.dateOfLoan, today);
-
  }
 
- public static String messageLoan (Book book) {
+ public static String messageLoan(Book book) {
      long Loan = Book.daysOfLoan(book);
      String message;
      if (Loan <=21)
--- src/BookDemo.java
+++ src/BookDemo.java
@@ -9,18 +9,15 @@ public class BookDemo {
         book1.title = "Java - steg för steg";
         book1.author = "Jan Skansholm";
         book1.price = 399;
-        book1.dateOfLoan = LocalDate.of(2020, Month.OCTOBER, 20 );
+        book1.dateOfLoan = LocalDate.of(2020, Month.OCTOBER, 1 );
         //WRITE NAME with help of method
         System.out.println(Book.getName(book1));
         //work with number of loan days
-
         System.out.println("Days of loan: "  + Book.daysOfLoan(book1) + " days");
-        //work with days of loan
-        /*long Loan = Book.messageLoan(Book);
-        System.out.println(Loan);
-        System.out.println("Loan: ", Book.messageLoan(book1));
+        //work with message about loaned book
+        String message = Book.messageLoan(book1);
+        System.out.println(" message: " + message);
 
-         */
 
     }
 }
