479fc2da70b0834dd1e9ed0d0ebf2467fef2087d
Vera Stepanova <stepa.vera@gmail.com>
2020-11-09 17:53:55 +0100

    updates

--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,40 @@
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * Created by Vera Stepanova
+ * Date: 2020-11-11
+ * Project Uppgift-3
+ */
 public class Book {
     String title;
     String author;
     LocalDate dateOfLoan;
 
-public static String getName(Book book){
-    return "Titel: " + book.title +
+    /**
+     * getLoan is a class method that returns title and author of the loaned book
+     * @param book is an object of class Book
+     * @return title and author (Title + Author)
+     */
+    public static String getBook(Book book){
+    return "Title: " + book.title +
             "\nAuthor: " + book.author;}
 
-public static long daysOfLoan(Book book) {
+    /**
+     * daysOfLoan är en klass method that counts
+     * number of days between day the loan of book was made and today's day
+     * @param book is a reference to the object of klass Book
+     * @return number of loan days
+     */
+    public static long daysOfLoan(Book book) {
     LocalDate today = LocalDate.now();
     return ChronoUnit.DAYS.between(book.dateOfLoan, today);
  }
-
+    /**
+     * messageLoan is a class method that returns message with the status of the loan
+     * @param book ia loaned book
+     * @return message
+     */
  public static String messageLoan(Book book) {
      long Loan = Book.daysOfLoan(book);
      String message;
@@ -23,8 +43,8 @@ public static long daysOfLoan(Book book) {
                  "\nYou have "+ (21-Loan) +" days remaining.";
      else
          message = "\nYou kan keep your book up to 21 days. "+ "Your loan is now "+
-                 (Loan - 21) + " days late. \nFee to pay: " + ((Loan-21)*20) +
-                 " SEK. \nPlease return your book or prolong your loan!";
+                 (Loan - 21) + " days late. \nFee to pay: " + ((Loan-21)*10) +
+                 " SEK. \nPlease return your book or pay the fee and extend your loan!";
      return message;
  }
 }
--- src/BookDemo.java
+++ src/BookDemo.java
@@ -8,15 +8,15 @@ public class BookDemo {
         //Change name, price or date of loan
         book1.title = "Java - steg för steg";
         book1.author = "Jan Skansholm";
-        book1.dateOfLoan = LocalDate.of(2020, Month.OCTOBER, 1 );
+        book1.dateOfLoan = LocalDate.of(2020, Month.OCTOBER, 22 );
         LocalDate today = LocalDate.now();
-        //write title and author of the book with help of method
+        //title and author of the book with help of method
         System.out.println("Today's date: " + today);
-        System.out.println(Book.getName(book1));
+        System.out.println(Book.getBook(book1));
         System.out.println("Day of loan: " + book1.dateOfLoan);
-        //work with number of loan days
-        System.out.println("Days of loan: "  + Book.daysOfLoan(book1) + " days");
-        //work with message about loaned book
+        //number of loan days
+        System.out.println("Total days of your loan: "  + Book.daysOfLoan(book1) + " days");
+        //message about loaned book
         String message = Book.messageLoan(book1);
         System.out.println("Status of your loan: " + message);
     }
